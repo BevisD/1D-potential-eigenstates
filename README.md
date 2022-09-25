72c9cda998c953e6e68de5ad594c84318b5ec6af
Bevis <bevisd@btinternet.com>
2022-09-25 13:58:52 +0100

    Plot Eigenstates of 1D potential

---
+++ README.md
@@ -0,0 +1,2 @@
+# 1D-potential-eigenstates
+Numerically Calculate the Eigenstates and Energies of a Bound Particle in a 1D Potential
--- main.py
+++ main.py
@@ -26,17 +26,21 @@ dx = x[1] - x[0]
 
 Es, states = get_eigenstates(x, V)
 
+fig = plt.figure(layout="constrained", figsize=(12, 6))
+subfigs = fig.subfigures(1, 2)
+axs0 = subfigs[0].subplots(4, 1, sharex=True)
+axs1 = subfigs[1].subplots(1, 1)
 
-fig, axs = plt.subplots(1, 2, figsize=(12,6))
-axs[0].set_title("$n_{th}$ Eigenstate in a\nQuadratic Potential")
+subfigs[0].suptitle("$n_{th}$ Eigenstate in a\nQuadratic Potential")
+subfigs[0].supxlabel("$x$")
+
 for i in range(4):
-    axs[0].plot(x[1:-1], states[i], label=f"$\psi_{i}$")
-axs[0].legend()
-axs[0].set(xlabel="$x$", ylabel="$\psi_n$")
+    axs0[i].plot(x[1:-1], states[i], label=f"$\psi_{i}$")
+    axs0[i].set(ylabel=f"$\psi_{i}$", ylim=(-2.5, 2.5))
 
-axs[1].plot(Es[:60], label="Simulated $E_n$")
-axs[1].plot(2 * (np.arange(0, 60) + 0.5) * omega, label=r"$(n+\frac{1}{2})\hbar\omega$")
-axs[1].set_title("Energy of $n_{th}$ eigenstate")
-axs[1].set(xlabel="$n$", ylabel="$E_n$")
-axs[1].legend()
+axs1.plot(Es[:60], label="Simulated $E_n$")
+axs1.plot(2 * (np.arange(0, 60) + 0.5) * omega, label=r"$(n+\frac{1}{2})\hbar\omega$")
+axs1.set_title("Energy of $n_{th}$ eigenstate")
+axs1.set(xlabel="$n$", ylabel="$E_n$")
+axs1.legend()
 plt.show()
